2133047dc6c4a25ce74f8ac44874fd3a8180f8a0
irem.erim <iremerim34@gmail.com>
2024-08-26 16:25:03 +0300

    location added

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -3,6 +3,7 @@ import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 export default function Contact() {
   const t = useTranslations('');
+  
   return (
     <div className='px-8 py-16 text-center text-white bg-gray-800'>
       <h1 className='text-3xl font-bold mb-4'>{t('Contact')}</h1>
@@ -10,6 +11,18 @@ export default function Contact() {
         <p className='mb-2'>{t('Address')}</p>
         <p className='mb-2'>{t('Phone')}</p>
         <p>{t('Email')}</p>
+      </div>
+      <div className='mb-8'>
+        <h2 className='text-2xl font-semibold mb-4'>{t('Location')}</h2>
+        <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d316.9286880322166!2d29.157489357765364!3d40.91243917705374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cac40c4badff33%3A0xc63c8d8188f68ce1!2sDAP%20DALGA%20KULE!5e0!3m2!1str!2str!4v1724678474901!5m2!1str!2str" 
+          width="600" 
+          height="450" 
+          style={{ border: 0 }} 
+          loading="lazy" 
+          referrerPolicy="no-referrer-when-downgrade"
+          className='w-full h-80'
+        ></iframe>
       </div>
       <div className='mt-8'>
         <h2 className='text-2xl font-semibold mb-4'>{t('Follow')}</h2>
